2a12bcf1adcae115e9914b00092074d1358d15ae
bjm321 <bjm321@users.noreply.github.com>
2020-02-25 06:21:15 -0800

    update to code

--- Robot_template/src/main.cpp
+++ Robot_template/src/main.cpp
@@ -1,33 +1,23 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       C:\Users\bjm32                                            */
-/*    Created:      Fri Feb 07 2020                                           */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// leftFrontMotor1      motor         1
-// leftFrontMotor2      motor         2
-// leftFrontMotor3      motor         3
-// leftBackMotor1       motor         4
-// leftBackMotor2       motor         5
-// leftBackMotor3       motor         6
-// rightFrontMotor1     motor         7
-// rightFrontMotor2     motor         8
-// rightFrontMotor3     motor         9
-// rightBackMotor1      motor         10
-// rightBackMotor2      motor         11
-// rightBackMotor3      motor         12
-// leftArmMotor         motor         13
-// rightArmMotor        motor         14
-// leftIntakeMotor      motor         15
-// rightIntakeMotor     motor         16
-// trayPivotMotor       motor         17
-// Controller1          controller
+
+// leftFrontDrMotor1    motor         1               
+// leftFrontDrMotor2    motor         2               
+// leftFrontDrMotor3    motor         3               
+// leftBackDrMotor1     motor         4               
+// leftBackDrMotor2     motor         5               
+// leftBackDrMotor3     motor         6               
+// rightFrontDrMotor1   motor         7               
+// rightFrontDrMotor2   motor         8               
+// rightFrontDrMotor3   motor         9               
+// rightBackDrMotor1    motor         10              
+// rightBackDrMotor2    motor         11              
+// rightBackDrMotor3    motor         12              
+// leftIntakeMotor      motor         15              
+// rightIntakeMotor     motor         16              
+// trayPivotMotor       motor         17              
+// Controller1          controller                    
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -41,9 +31,8 @@ void leftFrontwheel(int speed);
 void leftBackwheel(int speed);
 void rightFrontwheel(int speed);
 void rightBackwheel(int speed);
-void arm(int speed);
 void intake(int speed);
-void drive(int threshold);
+void driveBase(int threshold);
 int rightBackwheelvalue(int threshold);
 int rightFrontwheelvalue(int threshold);
 int leftBackwheelvalue(int threshold);
@@ -53,10 +42,8 @@ int Axis4value(int threshold);
 int Axis3value(int threshold);
 int speedControl();
 void driveStopMotor();
-void armStopMotor();
 void intakeStopMotor();
 int speedValueCheck(int speed);
-void arm(int speed, int runTime);
 void forwardByTime(int speed, int runTime);
 void reverseByTime(int speed, int runTime);
 void leftStrafeByTime(int speed, int runTime);
@@ -64,13 +51,11 @@ void rightStrafeByTime(int speed, int runTime);
 void rightInplaceTurnByTime(int speed, int runTime);
 void leftInplaceTurnByTime(int speed, int runTime);
 void driveClearEncoder();
-void armClearEncoder();
 void trayPivotClearEncoder();
 void leftFrontwheelRotate(int speed, int rotationNumber);
 void leftBackwheelRotate(int speed, int rotationNumber);
 void rightFrontwheelRotate(int speed, int rotationNumber);
 void rightBackwheelRotate(int speed, int rotationNumber);
-void armRotate(int speed, int rotationNumber);
 void forwardByDistance(int speed, int Distance);
 void reverseByDistance(int speed, int Distance);
 void leftStrafeByDistance(int speed, int Distance);
@@ -83,14 +68,7 @@ int main() {
   vexcodeInit();
   int speedarm = 100;
   while (true) {
-    drive(10);
-
-    if (Controller1.ButtonUp.pressing())
-    arm(speedarm / speedControl());
-  else if (Controller1.ButtonDown.pressing())
-    arm(speedarm / speedControl());
-  else
-    armStopMotor();
+    driveBase(10);
 
 if (Controller1.ButtonLeft.pressing())
     intake(speedarm / speedControl());
@@ -101,7 +79,7 @@ if (Controller1.ButtonLeft.pressing())
   }
 }
 
-void drive(int threshold) {
+void driveBase(int threshold) {
   leftFrontwheel(leftFrontwheelvalue(threshold));
   leftBackwheel(leftBackwheelvalue(threshold));
   rightFrontwheel(rightFrontwheelvalue(threshold));
@@ -109,36 +87,30 @@ void drive(int threshold) {
 }
 
 void leftFrontwheel(int speed) {
-  leftFrontMotor1.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
-  leftFrontMotor2.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
-  leftFrontMotor3.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
+  leftFrontDrMotor1.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
+  leftFrontDrMotor2.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
+  leftFrontDrMotor3.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
 }
 
 void leftBackwheel(int speed) {
-  leftBackMotor1.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
-  leftBackMotor2.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
-  leftBackMotor3.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
+  leftBackDrMotor1.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
+  leftBackDrMotor2.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
+  leftBackDrMotor3.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
 }
 
 void rightFrontwheel(int speed) {
-  rightFrontMotor1.spin(vex::directionType::fwd, speed,
+  rightFrontDrMotor1.spin(vex::directionType::fwd, speed,
                         vex::velocityUnits::pct);
-  rightFrontMotor2.spin(vex::directionType::fwd, speed,
+  rightFrontDrMotor2.spin(vex::directionType::fwd, speed,
                         vex::velocityUnits::pct);
-  rightFrontMotor3.spin(vex::directionType::fwd, speed,
+  rightFrontDrMotor3.spin(vex::directionType::fwd, speed,
                         vex::velocityUnits::pct);
 }
 
 void rightBackwheel(int speed) {
-  rightBackMotor1.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
-  rightBackMotor2.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
-  rightBackMotor3.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
-}
-
-void arm(int speed) { // needs work
-
-  rightArmMotor.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
-  leftArmMotor.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
+  rightBackDrMotor1.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
+  rightBackDrMotor2.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
+  rightBackDrMotor3.spin(vex::directionType::fwd, speed, vex::velocityUnits::pct);
 }
 
 void intake(int speed) { // needs work
@@ -192,23 +164,18 @@ int speedControl() {
 }
 
 void driveStopMotor() {
-  leftFrontMotor1.stop();
-  leftFrontMotor2.stop();
-  leftFrontMotor3.stop();
-  leftBackMotor1.stop();
-  leftBackMotor2.stop();
-  leftBackMotor3.stop();
-  rightFrontMotor1.stop();
-  rightFrontMotor2.stop();
-  rightFrontMotor3.stop();
-  rightBackMotor1.stop();
-  rightBackMotor2.stop();
-  rightBackMotor3.stop();
-}
-
-void armStopMotor() {
-  leftArmMotor.stop();
-  rightArmMotor.stop();
+  leftFrontDrMotor1.stop();
+  leftFrontDrMotor2.stop();
+  leftFrontDrMotor3.stop();
+  leftBackDrMotor1.stop();
+  leftBackDrMotor2.stop();
+  leftBackDrMotor3.stop();
+  rightFrontDrMotor1.stop();
+  rightFrontDrMotor2.stop();
+  rightFrontDrMotor3.stop();
+  rightBackDrMotor1.stop();
+  rightBackDrMotor2.stop();
+  rightBackDrMotor3.stop();
 }
 
 void intakeStopMotor() {
@@ -287,69 +254,56 @@ void leftInplaceTurnByTime(int speed, int runTime) {
 
 void driveClearEncoder() {
 
-  leftFrontMotor1.setPosition(0, degrees);
-  leftFrontMotor2.setPosition(0, degrees);
-  leftFrontMotor3.setPosition(0, degrees);
-  leftBackMotor1.setPosition(0, degrees);
-  leftBackMotor2.setPosition(0, degrees);
-  leftBackMotor3.setPosition(0, degrees);
-  rightFrontMotor1.setPosition(0, degrees);
-  rightFrontMotor2.setPosition(0, degrees);
-  rightFrontMotor3.setPosition(0, degrees);
-  rightBackMotor1.setPosition(0, degrees);
-  rightBackMotor2.setPosition(0, degrees);
-  rightBackMotor3.setPosition(0, degrees);
-}
-
-void armClearEncoder() {
-  rightArmMotor.setPosition(0, degrees);
-  leftArmMotor.setPosition(0, degrees);
+  leftFrontDrMotor1.setPosition(0, degrees);
+  leftFrontDrMotor2.setPosition(0, degrees);
+  leftFrontDrMotor3.setPosition(0, degrees);
+  leftBackDrMotor1.setPosition(0, degrees);
+  leftBackDrMotor2.setPosition(0, degrees);
+  leftBackDrMotor3.setPosition(0, degrees);
+  rightFrontDrMotor1.setPosition(0, degrees);
+  rightFrontDrMotor2.setPosition(0, degrees);
+  rightFrontDrMotor3.setPosition(0, degrees);
+  rightBackDrMotor1.setPosition(0, degrees);
+  rightBackDrMotor2.setPosition(0, degrees);
+  rightBackDrMotor3.setPosition(0, degrees);
 }
 
 void trayPivotClearEncoder() { trayPivotMotor.setPosition(0, degrees); }
 
 void leftFrontwheelRotate(int speed, int rotationNumber) {
-  leftFrontMotor1.setVelocity(speed, pct);
-  leftFrontMotor2.setVelocity(speed, pct);
-  leftFrontMotor3.setVelocity(speed, pct);
-  leftFrontMotor1.spinFor(rotationNumber, degrees);
-  leftFrontMotor2.spinFor(rotationNumber, degrees);
-  leftFrontMotor3.spinFor(rotationNumber, degrees);
+  leftFrontDrMotor1.setVelocity(speed, pct);
+  leftFrontDrMotor2.setVelocity(speed, pct);
+  leftFrontDrMotor3.setVelocity(speed, pct);
+  leftFrontDrMotor1.spinFor(rotationNumber, degrees);
+  leftFrontDrMotor2.spinFor(rotationNumber, degrees);
+  leftFrontDrMotor3.spinFor(rotationNumber, degrees);
 }
 
 void leftBackwheelRotate(int speed, int rotationNumber) {
-  leftBackMotor1.setVelocity(speed, pct);
-  leftBackMotor2.setVelocity(speed, pct);
-  leftBackMotor3.setVelocity(speed, pct);
-  leftBackMotor1.spinFor(rotationNumber, degrees);
-  leftBackMotor2.spinFor(rotationNumber, degrees);
-  leftBackMotor3.spinFor(rotationNumber, degrees);
+  leftBackDrMotor1.setVelocity(speed, pct);
+  leftBackDrMotor2.setVelocity(speed, pct);
+  leftBackDrMotor3.setVelocity(speed, pct);
+  leftBackDrMotor1.spinFor(rotationNumber, degrees);
+  leftBackDrMotor2.spinFor(rotationNumber, degrees);
+  leftBackDrMotor3.spinFor(rotationNumber, degrees);
 }
 
 void rightFrontwheelRotate(int speed, int rotationNumber) {
-  rightFrontMotor1.setVelocity(speed, pct);
-  rightFrontMotor2.setVelocity(speed, pct);
-  rightFrontMotor3.setVelocity(speed, pct);
-  rightFrontMotor1.spinFor(rotationNumber, degrees);
-  rightFrontMotor2.spinFor(rotationNumber, degrees);
-  rightFrontMotor3.spinFor(rotationNumber, degrees);
+  rightFrontDrMotor1.setVelocity(speed, pct);
+  rightFrontDrMotor2.setVelocity(speed, pct);
+  rightFrontDrMotor3.setVelocity(speed, pct);
+  rightFrontDrMotor1.spinFor(rotationNumber, degrees);
+  rightFrontDrMotor2.spinFor(rotationNumber, degrees);
+  rightFrontDrMotor3.spinFor(rotationNumber, degrees);
 }
 
 void rightBackwheelRotate(int speed, int rotationNumber) {
-  rightBackMotor1.setVelocity(speed, pct);
-  rightBackMotor2.setVelocity(speed, pct);
-  rightBackMotor3.setVelocity(speed, pct);
-  rightBackMotor1.spinFor(rotationNumber, degrees);
-  rightBackMotor2.spinFor(rotationNumber, degrees);
-  rightBackMotor3.spinFor(rotationNumber, degrees);
-}
-
-void armRotate(int speed, int rotationNumber) {
-
-  rightArmMotor.rotateTo(rotationNumber, degrees);
-  leftArmMotor.rotateTo(rotationNumber, degrees);
-  rightArmMotor.setVelocity(speed, pct);
-  leftArmMotor.setVelocity(speed, pct);
+  rightBackDrMotor1.setVelocity(speed, pct);
+  rightBackDrMotor2.setVelocity(speed, pct);
+  rightBackDrMotor3.setVelocity(speed, pct);
+  rightBackDrMotor1.spinFor(rotationNumber, degrees);
+  rightBackDrMotor2.spinFor(rotationNumber, degrees);
+  rightBackDrMotor3.spinFor(rotationNumber, degrees);
 }
 
 void forwardByDistance(int speed, int Distance) {
